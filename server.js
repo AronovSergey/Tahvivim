3b489cc314c951f46a3fdce9598714fd0c5ecec0
Sergey <aronovsergeyy@gmail.com>
2021-04-29 13:43:21 +0300

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express();
 const path = require("path");
+
+const app = express();
+
 app.use(cors());
 app.get("/api/getUser", (req, res) => {
 	const user = "Evgeni";
@@ -9,6 +12,20 @@ app.get("/api/getUser", (req, res) => {
 });
 
 const port = 8000;
+
+//Connect to db with mongoose
+const uri =
+	"mongodb+srv://sergey:1234@appleseeds.yk0bc.mongodb.net/Tahvivim?retryWrites=true&w=majority";
+mongoose
+	.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("Database connect");
+	});
 
 if (process.env.NODE_ENV === "production") {
 	// Exprees will serve up production assets
