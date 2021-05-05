72816dee151da5d7be32bf750c34ca4a1b8b3f03
Sergey <aronovsergeyy@gmail.com>
2021-05-05 23:51:18 +0300

    added a menu section

--- client/src/components/header/HeaderTitle.tsx
+++ client/src/components/header/HeaderTitle.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme) => ({
-	title: {
-		display: "none",
-		[theme.breakpoints.up("sm")]: {
-			display: "block",
-		},
-	},
-}));
-
 const HeaderTitle = () => {
-	const classes = useStyles();
 	const history = useHistory();
 
 	return (
-		<Typography
-			className={classes.title}
-			variant="h4"
-			noWrap
-			onClick={() => history.push("/")}
-		>
+		<Typography variant="h4" noWrap onClick={() => history.push("/")}>
 			Tahvivim
 		</Typography>
 	);
